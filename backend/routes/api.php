a95bb8a6ca43fe4328c475186d92bd25eee876fb
Patricia Soares <patfatec@gmail.com>
2026-09-02 16:20:38 -0300

    feat(api): implementar apiResource para CRUD de recados

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,10 +21,14 @@ Route::post('/login',    [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
+    Route::apiResource('recados', RecadoController::class);
+    /*
     Route::get('/recados', [RecadoController::class, 'index']);
     Route::post('/recados', [RecadoController::class, 'store']);
     Route::put('/recados/{id}',    [RecadoController::class, 'update']);
     Route::delete('/recados/{id}', [RecadoController::class, 'destroy']);
-
+    */
+    
     Route::post('/logout', [AuthController::class, 'logout']);
+  
 });
