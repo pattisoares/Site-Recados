315ef4c4d0a297287abe2dcc35a4c8a8caebdb81
Who <matheus.dd07@gmail.com>
2026-09-22 22:23:18 -0300

    feat: adiciona o arquivo recadoItem

---
+++ frontend/src/pages/recadoItem.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import ExcluirRecado from './excluirRecado.js';
+import EditarRecado from './editarRecado.js';
+
+function RecadoItem({ recado, onExcluir, onEditar }) {
+  const [editando, setEditando] = useState(false);
+
+  return (
+    <li className="recado-item">
+      {!editando && (
+        <>
+          <h3>{recado.titulo}</h3>
+          <p>{recado.descricao}</p>
+        </>
+      )}
+
+      <EditarRecado
+        recado={recado}
+        onEditar={onEditar}
+        editando={editando}
+        setEditando={setEditando}
+      />
+
+      {!editando && (
+        <div className="recado-acoes">
+          <ExcluirRecado id={recado.id} onExcluir={onExcluir} />
+        </div>
+      )}
+    </li>
+  );
+}
+
+export default RecadoItem;
